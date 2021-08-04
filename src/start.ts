0e6fb07a3db3dbabf9042ad0cc45a0cf770f5fff
bgordon8 <gordonbrian16@gmail.com>
2021-08-04 15:04:20 -0600

    Add express config, connect typescript

---
+++ src/start.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+export default async function start() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: gql`
+      type Query {
+        helloWorld: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        helloWorld: () => "Hello World, it's me again",
+      },
+    },
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+
+  return app.listen(3000, () => {
+    console.log('listening on port 3000...');
+  });
+}
